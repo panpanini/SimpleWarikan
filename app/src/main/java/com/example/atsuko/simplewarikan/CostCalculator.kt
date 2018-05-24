32740435fde32366a381d3a7247a373c4d0a094a
liconrpeo <liconrepo@mercari.com>
2018-05-24 15:34:21 +0900

    calculatorCost modified to pass the tests

--- app/src/main/java/com/example/atsuko/simplewarikan/CostCalculator.kt
+++ app/src/main/java/com/example/atsuko/simplewarikan/CostCalculator.kt
@@ -2,8 +2,21 @@ package com.example.atsuko.simplewarikan
 
 class CostCalculator {
 
-
     fun calculateCost(cost: String, peopleCount: String): String {
-        return (cost.toInt() / peopleCount.toInt()).toString()
+        return try {
+            if ((peopleCount.toInt() > -1) && cost.toDouble() > -1) {
+                val result = (cost.toDouble() / peopleCount.toInt())
+                val divider = result.toInt()
+                if ((result.rem(divider)) == (0.0)) {
+                    return result.toInt().toString()
+                } else {
+                    result.toString()
+                }
+            } else {
+                ""
+            }
+        } catch (exception: NumberFormatException) {
+            ""
+        }
     }
 }
